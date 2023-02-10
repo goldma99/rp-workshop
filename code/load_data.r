f7bb7a5d68043e154423901306ca890b6c1b9e57
ruoyuc99 <rchen4@uchicago.edu>
2023-02-10 10:44:51 -0600

    calculate average age

--- code/load_data.r
+++ code/load_data.r
@@ -1,2 +1,8 @@
+home_dir <- "C:/Users/rchen4/Dropbox/Projects/rp-workshop"
+child_analysis <- read.csv(paste0(home_dir,"/data/analysis/child.csv"))
+saveRDS(child_analysis,file=paste0(home_dir,"/data/analysis/child.rds"))
 
-child_analysis <- read.csv("child.csv")
+mother_tidy <- read.csv(paste0(home_dir,"/data/tidy/mother.csv"))
+
+avg_age_child <- mean(child_analysis$age)
+avg_age_mother <- mean(mother_tidy$age)
