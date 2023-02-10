bcfd5e2c6e8657855530366222f509ebc7b9efff
Jieyi-Chen-98 <jieyi@uchicago.edu>
2023-02-10 11:16:45 -0600

    jc
    jc

--- data/raw/data_ingestion_JC.R
+++ data/raw/data_ingestion_JC.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(skimr)
-setwd("/Users/chenjieyi/Desktop/rp-workshop/data/raw")
+setwd("/Users/chenjieyi/Documents/GitHub/rp-workshop/data/raw")
 mother_long <- read_csv("mother_long.csv")
 # save RDS
 saveRDS(mother_long, file = "mother_long.RDS")
@@ -21,3 +21,22 @@ load("mother_survey.RDS")
 
 skim(mother_survey)
 summary(mother_survey)
+
+# Tidy Data ----
+setwd("/Users/chenjieyi/Documents/GitHub/rp-workshop")
+
+mother_survey <- read_csv("data/raw/mother_survey.csv")
+child <- read_csv("data/tidy/child.csv")
+
+mother_survey %>% 
+  pivot_longer(cols = age_1:age_7, names_to = "child", values_to = "age_child") %>% 
+  summarise(mean(age_child, na.rm=TRUE))
+  
+child %>% 
+  summarise(mean(age, na.rm = TRUE))
+
+mother_long <- read_csv("data/raw/mother_long.csv")
+mother <- read_csv("data/tidy/mother.csv")
+child_an <- read_csv("data/analysis/child.csv")
+
+
